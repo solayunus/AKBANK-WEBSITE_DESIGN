467230413b59a8b6231244c007cef2e9d4db90c2
YunusBinKhalid <solayunus19@gmail.com>
2019-10-16 13:07:15 +0100

    New Setting included

---
+++ slider.js
@@ -0,0 +1,29 @@
+
+
+var  slideIndex= 0;
+var myslides = document.querySelector('.item1');
+myslides.style.backgroundImage ="url(images/main_campaign_splash_affluent_banking.jpg)"
+
+ const imgArr = [ 'images/main_campaign_splash_affluent_banking.jpg', 
+//                 'images/main_campaign_splash_ayricaliklar_dunyasi_en.jpg',
+//                 'images/main_campaign_splash_affluent_banking.jpg',
+//                 'images/item1.jpg'
+//             ];
+
+// function showslide() {
+//     var myslides = document.querySelector('.item1');
+    
+   
+// for(i=0; i< imgArr.length; i++){
+//     slideIndex++;
+//      if(slideIndex >= 4)
+//     {
+//         slideIndex = slideIndex -1; }
+//     let imgshow = imgArr[slideIndex];
+//     console.log (imgshow);
+// myslides.style.backgroundImage = "url(imgshow)";
+// }
+// setTimeout(showslide, 10000);
+// }
+
+// showslide();
